25da4db94802cda6a7b30efb9d6a6906391d0f1f
Jean Paul <jeanpaul.puerta@utp.edu.co>
2024-03-28 16:27:12 -0500

    finish design login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,14 +8,14 @@ const Login = () => {
       {//Primer Div, de la derecha
       }
       <div className=" h-80 -mx-8 min-h-screen flex items-center justify-start px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full mx-auto bg-white shadow-md rounded-lg p-8">
-          <h2 className="text-center text-3xl font-extrabold text-gray-900">Iniciar sesión en tu cuenta</h2>
+            <div className="max-w-md w-full mx-auto bg-white drop-shadow-2xl rounded-lg p-8">
+          <h2 className="text-center text-5xl font-extrabold text-gray-900">Iniciar sesión</h2>
           <form className="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
                 <label htmlFor="email-address" className="sr-only">Correo electrónico</label>
-                <input id="email-address" name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Correo electrónico" />
+                <input id="email-address" name="email" type="email" autoComplete="email" required className="mb-4 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Correo electrónico" />
               </div>
               <div>
                 <label htmlFor="password" className="sr-only">Contraseña</label>
@@ -47,7 +47,7 @@ const Login = () => {
             </div>
           </form>
           <div>
-            <p className="text-center text-sm text-gray-600">
+            <p className="text-center text-sm text-gray-600 pt-5">
               ¿No tienes una cuenta? <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">Crear una cuenta</a>
             </p>
           </div>
@@ -59,12 +59,14 @@ const Login = () => {
       {//Segundo Div, de la derecha
       }
 
-      <div className="-mx-8 min-h-screen flex items-center justify-end px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full mx-auto bg-white shadow-md rounded-lg p-8">
-        <img className="w-80 h-80" src="./public/imglogin.svg"/>
+<section className="-mx-8 min-h-screen flex items-center justify-end px-4 sm:px-6 lg:px-8">
+  <div className="grid max-w-md w-full mx-auto bg-white shadow-md rounded-lg p-8">
+    <h1 className="text-xs font-bold text-gray-900 ml-auto">Descubre más acerca de nosotros</h1>
+    <button className="mt-2 w-16 text-xs font-bold py-1 px-2 border border-indigo-600 rounded-md text-indigo-600 bg-white hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-auto">Vamos</button>
+    <img className="w-72 h-72" src="./public/imglogin.svg"/>
+  </div>
+</section>
 
-        </div>
-      </div>
     </div>
 
   </div>
