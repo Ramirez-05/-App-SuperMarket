c5544694f0e1f698537fd1d202f4455e1bf487a7
Jean Paul <jeanpaul.puerta@utp.edu.co>
2024-04-02 20:28:00 -0500

    Co-authored-by: Ramirez-05 <Ramirez-05@users.noreply.github.com>

--- API-MARKET/Api/crud/person.py
+++ API-MARKET/Api/crud/person.py
@@ -1,6 +1,6 @@
 from Api.models.person import Person
 from sqlalchemy.orm import Session
-from Api.schemas.person import PersonBase
+from Api.schemas.person import PersonBase, PersonRead
 from Api.schemas.role import RoleRead
 from Api.schemas.user import UserBase
 from Api.crud.user import create_new_user
@@ -26,5 +26,25 @@ def create_new_person_user(persona: PersonBase,user:UserBase,role:RoleRead ,db:S
         print(f"error al crear persona: {str(e)}",file=sys.stderr)
         raise HTTPException(status_code=500,detail=f"no se pudo agregar la persona: {str(e)}")
 
+###############################################################################################################
+    
+def get_person_by_cedula(persona: PersonBase ,db:Session):
+    person = db.query(Person).filter(Person.cedula == persona.cedula).first()
+    return person
 
-
+def update_person(persona: PersonBase, db: Session):
+    db_person = get_person_by_cedula(persona, db)
+    if db_person is None:
+        raise HTTPException(status_code=404, detail="La persona no existe")
+    try:
+        db_person.nombres = persona.nombres
+        db_person.apellidos = persona.apellidos
+        db_person.direccion = persona.direccion
+        db_person.telefono = persona.telefono
+        db.commit()
+        db.refresh(db_person)
+        return db_person
+    except Exception as e:
+        db.rollback()
+        print(f"error al actualizar persona: {str(e)}",file=sys.stderr)
+        raise HTTPException(status_code=500,detail=f"no se pudo actualizar la persona: {str(e)}")    
--- API-MARKET/Api/routes/person.py
+++ API-MARKET/Api/routes/person.py
@@ -1,18 +1,18 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from Api.crud.person import create_new_person_user
-from Api.schemas.person import PersonBase
+from Api.crud.person import create_new_person_user, update_person
+from Api.schemas.person import PersonBase, PersonRead
 from Api.schemas.role import RoleRead
 from Api.schemas.user import UserBase
-
 from db.connection import get_session
 
 router = APIRouter()
 
-@router.post("/addperson", response_model=PersonBase)
+@router.post("/create-persona", response_model=PersonBase)
 async def add_person(persona: PersonBase,user:UserBase,role:RoleRead ,db: Session = Depends(get_session)):
-    print("persona",persona)
-    print("user",user.correo,".........")
-    print("user",user.contrasena)
-    print("role",role)
     return create_new_person_user(persona,user,role,db)
+
+
+@router.put("/update-persona", response_model=PersonBase)
+async def fuction_update_person(persona: PersonBase, db:Session = Depends(get_session)):
+    return update_person(persona, db)
