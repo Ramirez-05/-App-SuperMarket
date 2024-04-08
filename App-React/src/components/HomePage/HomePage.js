780c3719f3d4da62094cc5cf8434e677b9688ee3
jeanpaul615 <127168141+jeanpaul615@users.noreply.github.com>
2024-04-08 12:34:32 -0500

    Update HomePage.js

--- App-React/src/components/HomePage/HomePage.js
+++ App-React/src/components/HomePage/HomePage.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 export default function HomePage() {
     return ( 
-        <div className="flex justify-center">
-            <h1 className="text-3xl font-bold">
-                HOLA GENTE
+    <div className="">
+        {/*Navbar login-registrarse*/}
+        <div className="flex justify-end mr-20 mt-5">
+            <Link to='/login'>
+            <h1 className="text-base bg-lime-700 text-white font-bold mr-10 p-1 hover:bg-slate-100 hover:text-lime-700">
+                INICIAR SESION
             </h1>
+            </Link> 
+
+            <Link to='/createaccount'>
+            <h1 className="text-base text-white font-bold bg-cyan-600 p-1 hover:bg-slate-100 hover:text-cyan-600">
+            REGISTRARSE
+            </h1>
+            </Link>
         </div>
+
+
+        <div className="flex justify-center mt-14">
+        <h1 className="text-5xl font-black">
+            SISTEMA DE INVENTARIO
+        </h1>
+        </div>
+    </div>
     );
 }
