aa9b3f60cb83fb2a0860f779b693c608de3e07f1
Jean Paul <jeanpaul.puerta@utp.edu.co>
2024-05-03 16:23:53 -0500

    cree las api para ver los usuarios disabled y active

--- Back - Python/Api/crud/user.py
+++ Back - Python/Api/crud/user.py
@@ -1,6 +1,6 @@
 from Api.models.user import User
 from sqlalchemy.orm import Session
-from Api.schemas.user import UserCreate,GetUser
+from Api.schemas.user import UserCreate,GetUser, UserRead
 from fastapi import HTTPException
 from core.security import get_hashed_password
 from core.utils import generateuser_id
@@ -33,4 +33,21 @@ def get_users(getuser: GetUser,db: Session):
         except Exception as e:
             print(f"Error al obtener usuarios: {str(e)}", file=sys.stderr)
             raise HTTPException(status_code=500, detail=f"No se pudo obtener usuarios: {str(e)}")
-     
+
+
+def disabled_users(db: Session):
+        try:
+            disabled_users = db.query(User).filter(User.id_role == 2).filter(User.estado == False).all()
+            return disabled_users
+        except Exception as e:
+            print(f"Error al obtener usuarios: {str(e)}", file=sys.stderr)
+            raise HTTPException(status_code=500, detail=f"No se pudo obtener usuarios: {str(e)}")
+        
+def active_users(db: Session):
+        try:
+            active_users = db.query(User).filter(User.id_role == 2).filter(User.estado == True).all()
+            return active_users
+        except Exception as e:
+            print(f"Error al obtener usuarios: {str(e)}", file=sys.stderr)
+            raise HTTPException(status_code=500, detail=f"No se pudo obtener usuarios: {str(e)}")
+            
--- Back - Python/Api/routes/users.py
+++ Back - Python/Api/routes/users.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from db.connection import get_session
 from sqlalchemy.orm import Session
 from Api.schemas.user import GetUser
-from Api.crud.user import get_users
+from Api.crud.user import get_users,disabled_users,active_users
 
 
 router = APIRouter()
@@ -10,4 +10,14 @@ router = APIRouter()
 @router.post("/get-users")
 async def get_all_users(getuser: GetUser = Depends(GetUser),db: Session = Depends(get_session)):
         print("Entro a la funcion obtener usuarios")
-        return get_users(getuser,db)                 
+        return get_users(getuser,db)         
+
+@router.get("/get-user-disabled")    
+async def get_user_disabled(db: Session = Depends(get_session)):
+        print("Entro a la funcion obtener usuarios desactivados") 
+        return disabled_users(db)
+
+@router.get("/get-user-active")    
+async def get_user_active(db: Session = Depends(get_session)):
+        print("Entro a la funcion obtener usuarios activados") 
+        return active_users(db)
