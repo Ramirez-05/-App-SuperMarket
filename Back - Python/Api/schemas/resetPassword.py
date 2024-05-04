642eaf5691bf3b9d1b2d39b8fbd98bc4573c2447
Ramirez-05 <117038114+Ramirez-05@users.noreply.github.com>
2024-05-04 11:02:32 -0500

    Organizando Proyecto

--- Back - Python/Api/schemas/resetPassword.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel
-
-class ResetPassword(BaseModel):
-    email: str
--- Back - Python/Api/schemas/role.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class RoleBase(BaseModel):
-    nombre: str
-    descripcion: str
-
-class RoleRead(BaseModel):
-    id_role: int
-    
