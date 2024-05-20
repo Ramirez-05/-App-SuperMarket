ee999dede8fa1cf757744e2974e4d077d92636e6
Jean Paul <jeanpaul.puerta@utp.edu.co>
2024-05-19 21:11:23 -0500

    estilo datatable

--- Front - React/src/components/Dashboard/Products.js
+++ Front - React/src/components/Dashboard/Products.js
@@ -25,14 +25,13 @@ const TableComponent = () => {
   useEffect(() => {
     if (products.length > 0) {
       if (!tableRef.current) {
-        // Inicializar DataTable solo si aún no está inicializado
         const table = $('#myTable').DataTable({
           paging: true,
           searching: true,
           info: true,
           lengthMenu: [5, 10, 25, 50, 100],
           responsive: true,
-          data: products, // Asignar los datos al inicializar DataTable
+          data: products,
           columns: [
             { data: 'nombre' },
             { data: 'precio' },
@@ -46,7 +45,6 @@ const TableComponent = () => {
     }
 
     return () => {
-      // No destruir la tabla DataTable al desmontar el componente
       if (tableRef.current) {
         tableRef.current.destroy(false);
         tableRef.current = null;
@@ -55,27 +53,29 @@ const TableComponent = () => {
   }, [products]);
 
   return (
-    <div>
-      <table id="myTable" className="display">
-        <thead className="bg-gray-50">
-          <tr>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Precio</th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Descripción</th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID Categoría</th>
-          </tr>
-        </thead>
-        <tbody className="bg-white divide-y divide-gray-200">
-          {products.map(product => (
-            <tr key={product.id_producto}>
-              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{product.nombre}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.precio}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.descripcion}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.id_categoria}</td>
+    <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600">
+      <div className="w-full max-w-7xl">
+        <table id="myTable" className="display mx-auto min-w-full bg-white rounded-lg shadow overflow-hidden">
+          <thead className="bg-gray-800 text-white">
+            <tr>
+              <th className="px-6 py-3 text-center text-sm font-bold uppercase tracking-wider">Nombre</th>
+              <th className="px-6 py-3 text-center text-sm font-bold uppercase tracking-wider">Precio</th>
+              <th className="px-6 py-3 text-center text-sm font-bold uppercase tracking-wider">Descripción</th>
+              <th className="px-6 py-3 text-center text-sm font-bold uppercase tracking-wider">ID Categoría</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {products.map(product => (
+              <tr key={product.id_producto}>
+                <td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium text-gray-900">{product.nombre}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-left text-sm text-gray-500">{product.precio}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-left text-sm text-gray-500">{product.descripcion}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-left text-sm text-gray-500">{product.id_categoria}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
